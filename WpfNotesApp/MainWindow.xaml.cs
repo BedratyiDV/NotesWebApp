c80247865a1a88f2394d16db0fa765c88c41dde2
BedratyiDV <bedrik2023@gmail.com>
2024-02-10 22:49:58 +0100

    WPF Project added
    10/02/2024

---
+++ WpfNotesApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NotesProcessor;
+using NoteDAL;
+using NoteContracts;
+
+
+namespace WpfNotesApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private NoteProcessor _noteProc;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _noteProc = new NoteProcessor(new NoteRepository());
+           
+        }
+
+        private void ShowNotesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainGrid.ItemsSource = _noteProc.GetAll();
+        }
+
+    }
+}
